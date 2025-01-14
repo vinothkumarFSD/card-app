38fa812b39a103a05defab7cc06f73055d835d60
vinothkumarFSD <vinothkumarj4140@gmail.com>
2025-01-14 18:27:56 +0530

    react task add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
+import React from "react";
+
 import './App.css';
 
-//normal import method using 'curly braces{}'
-//import { Card } from './Card';
-
-// default import method without using {curly braces}
 import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <Card cardName="FName"/>
-      <Card cardName="Age" inpType="number"/>
-      <Card cardName="DOB" inpType="date"/>
+      <Card />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,49 +1,82 @@
 import React from "react";
 
-//normal export method using export to beginning of the variable
-
-// //export const Card=()=>{
-// return (
-//     <div className="cardContainer">
-//         <label htmlFor="some-text">name</label>
-//         <input type="text" id="some-text"/>
-//     </div>
-// );
-
-// default export method using export in the end 
-
-// //const Card = () => {
-// const name= 'Some Text :';
-// return (
-//     <div className="cardContainer">
-//         <label htmlFor="some-text">name</label>
-//         <input type="text" id="some-text"/>
-//     </div>
-// );
-//export default Card;
-
-//without destructure using to pass the attribute of props--> this is not recommened way
-// props is only for first argument using,
-// here input type doesnt change only taken by first argument type(ex:text)
-
-// //const Card = (props) => {
-//     return (
-//         <div className="cardContainer">
-//             <label htmlFor={props.cardName}>{props.cardName}:</label>
-//             <input type="text" id={props.cardName} />
-//         </div>
-//     );
-// }
-
-// export default Card;
-
-//with destructure-->this is efficient recommended way
-const Card = ({cardName,inpType="text"}) => {
+const Card = () => {
     return (
-        <div className="cardContainer">
-            <label htmlFor={cardName}>{cardName}:</label>
-            <input type={inpType} id={cardName} />
-        </div>
+        <section className="pricing py-5">
+            <div className="container">
+                <div className="row">
+                    {/* first column */}
+                    <div className="col-lg-4">
+                        <div className="card mb-5 mb-lg-0">
+                            <div className="card-body">
+                                <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
+                                <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+                                <hr />
+                                <ul className="fa-ul">
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                                    <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>
+                                    <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>
+                                    <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain</li>
+                                    <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+                                </ul>
+                                <div className="d-grid text-center">
+                                    <a href="#" className="btn btn-primary btn-block text-uppercase">Button</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* second column */}
+                    <div className="col-lg-4">
+                        <div className="card mb-5 mb-lg-0">
+                            <div className="card-body">
+                                <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+                                <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
+                                <hr />
+                                <ul className="fa-ul">
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>5 User</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain</li>
+                                    <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+                                </ul>
+                                <div className="d-grid text-center">
+                                    <a href="#" className="btn btn-primary btn-block text-uppercase">Button</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* third column */}
+                    <div className="col-lg-4">
+                        <div className="card mb-5 mb-lg-0">
+                            <div className="card-body">
+                                <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
+                                <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
+                                <hr />
+                                <ul className="fa-ul">
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited User</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Free Subdomain</li>
+                                    <li><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+                                </ul>
+                                <div className="d-grid text-center">
+                                    <a href="#" className="btn btn-primary btn-block text-uppercase">Button</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
